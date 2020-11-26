83a8a327437f468f1c90fb1ff6c865a1b53a2ff0
Huong Giang Vo <51000008+HuongGiangVo@users.noreply.github.com>
2020-11-26 15:45:48 -0500

    Create EmployeeService.java

---
+++ src/main/java/cruddemo/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.springboot.cruddemo.entiry.Employee;
+
+public interface EmployeeService {
+	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+	public void deleteById(int theId);
+
+}
